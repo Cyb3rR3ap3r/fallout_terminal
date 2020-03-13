55a6ab657df630836cb1631b98371e48cf02b91f
root <root@localhost.localdomain>
2020-03-13 13:49:03 -0400

    Updated

--- menu.py
+++ menu.py
@@ -30,10 +30,6 @@ def boot():
 def menu():
 	boot()
 	os.system('clear')
-	#term_size = os.get_terminal_size()
-	#print(term_size)
-	#s = "Test"
-	#print(s.center(0))
 
 	menu_title = "WELCOME TO ROBCO INDUSTRIES (TM) TERMLINK\nTHANK YOU FOR CHOOSING VAULT-TEC!\n"
 	menu_items = ["[INIT PIP-BOY 2000 MK VI]", "[S.P.E.C.I.A.L]", "[VAULT-TEC MEDIA]", "[LOG OFF]"]
@@ -41,14 +37,20 @@ def menu():
 	cursor_style = ("bold",)
 	menu_style = ("standout",)
 	menu_exit = False
-
 	main_menu = TerminalMenu(menu_entries=menu_items, title=menu_title, menu_cursor=cursor, menu_cursor_style=cursor_style, menu_highlight_style=menu_style, cycle_cursor=True)
+
 
 	spec_menu_title = "WELCOME TO ROBCO INDUSTRIES (TM) TERMLINK\nTHANK YOU FOR CHOOSING VAULT-TEC!\n"
 	spec_menu_items = ["[STRENGTH]", "[PERCEPTION]", "[ENDURANCE]", "[CHARISMA]", "[INTELLIGENCE]", "[AGILITY]", "[LUCK]", "[GO BACK..]"]
 	spec_menu_back = False
-
 	spec_menu = TerminalMenu(spec_menu_items, spec_menu_title, cursor, cursor_style, menu_style)
+
+	strength = open('extras/strength_ascii.txt', 'r')
+	strength_contents = strength.read()
+	strength_menu_title = ("WELCOME TO ROBCO INDUSTRIES (TM) TERMLINK\nTHANK YOU FOR CHOOSING VAULT-TEC!\n" + strength_contents + "\n")
+	strength_menu_items = ["WATCH VIDEO", "[GO BACK..]"]
+	strength_menu_back = False
+	strength_menu = TerminalMenu(strength_menu_items, strength_menu_title, cursor, cursor_style, menu_style)
 
 	time.sleep(5)
 
@@ -64,20 +66,20 @@ def menu():
 				os.system('clear')
 				spec_sel = spec_menu.show()
 				if spec_sel == 0:
-					os.system('clear')
-					strength = open('ascii-art.html', 'r')
-					strength_contents = strength.read()
-					print(strength_contents)
-					time.sleep(5)
-					strength.close()
+					while not strength_menu_back:
+						os.system('clear')
+						strength_sel = strength_menu.show()
+						if strength_sel == 0:
+							print("Watch Video")
+						elif strength_sel == 1:
+							strength.close()
+							strength_menu_back = True
 				elif spec_sel == 1:
 					print("Save Selected")
 				elif spec_sel == 2:
 					spec_menu_back = True
 					print("Back Selected")
-			edit_menu_back = False
-			print("Option 2 Selected")
-			time.sleep(5)
+				strength_menu_back = False
 		elif main_sel == 2:
 			print("Option 3 Selected")
 			time.sleep(5)
